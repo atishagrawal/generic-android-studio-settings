270ee12c18da431fbb5b73cee0c42bc44f3a93c5
Atish Agrawal <dir.atishagrawal@gmail.com>
2021-09-02 11:09:17 +0530

    AI-Arctic Fox | 2020.3.1 Patch 2 <atish@ATISH-PC Create CodeGlance.xml, debugger.xml, visualizationTool.xml, colors.scheme.xml, kotlinRefactoring.xml, updates.xml, RecyclerViewAdapter.kt, rainbow_brackets.xml, keymap.xml, github.xml, editor.xml, Default.xml, ide.general.xml, project.default.xml, baseRefactoring.xml, File Header.java, KotlinHeader.kt, wsl.distributions.xml, pluginAdvertiser.xml, _@user_Darcula.icls, markdown.xml, path.macros.xml, androidStudioFirstRun.xml, screenRecorderOptions.xml, laf.xml, Windows Proper Redo.xml, ui.lnf.xml, web-browsers.xml, git.xml, Default.xml, jdk.table.xml, lightEdit.xml, notifications.xml, filetypes.xml, editor.codeinsight.xml, vcs.xml, plugin_ui.xml, find.xml, parameter.hints.xml

---
+++ fileTemplates/includes/KotlinHeader.kt
@@ -0,0 +1,9 @@
+#set ($USER = "Atish Agrawal")
+#set ($EMAIL = "atishagrawal@countrydelight.in, dir.atishagrawal@gmail.com")
+#set ($PHONE = "+91-9716987018")
+
+/**
+ * Created by ${USER} on ${DATE}.
+ * Email: ${EMAIL}
+ * Phone: ${PHONE}
+ */
